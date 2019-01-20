4e9f88b2ff74ecc11f903eabf2c9e5648f3befff
Ilya Zadorozhny <ilya.zadorozhny@gmail.com>
2019-01-21 00:24:06 +0300

    clan format change

--- src/utils/wrapper.cpp
+++ src/utils/wrapper.cpp
@@ -3,27 +3,26 @@
 namespace sba {
 namespace utils {
 
-    template <typename T>
-    void swap(T* const a, T* const b) {
-        T temp = *a; // call copy constructor
-        *a = *b; // call copy assign operator
-        *b = temp; // call copy assign operator
-    }
-
-    template <typename T>
-    void swap(const T& a, const T& b) {
-        T temp = a; // call copy constructor
-        a = b; // call copy assign operator
-        b = temp; // call copy assign operator
-    }
-
-    template <typename T>
-    void swap_optimal(const T& a, const T& b) {
-        T temp = a; // call copy constructor
-        a = b; // call copy assign operator
-        b = std::move(temp); // call copy replacement operator
-    }
-
-    
+template <typename T>
+void swap(T *const a, T *const b) {
+    T temp = *a; // call copy constructor
+    *a = *b;     // call copy assign operator
+    *b = temp;   // call copy assign operator
 }
+
+template <typename T>
+void swap(const T &a, const T &b) {
+    T temp = a; // call copy constructor
+    a = b;      // call copy assign operator
+    b = temp;   // call copy assign operator
 }
+
+template <typename T>
+void swap_optimal(const T &a, const T &b) {
+    T temp = a;          // call copy constructor
+    a = b;               // call copy assign operator
+    b = std::move(temp); // call copy replacement operator
+}
+
+} // namespace utils
+} // namespace sba
--- src/utils/wrapper.h
+++ src/utils/wrapper.h
@@ -6,14 +6,13 @@ namespace sba {
 namespace utils {
 
 template <typename T>
-void swap(T* const a, T* const b);
+void swap(T *const a, T *const b);
 
 template <typename T>
-void swap(const T& a, const T& b);
+void swap(const T &a, const T &b);
 
 template <typename T>
-void swap_optimal(const T& a, const T& b);
+void swap_optimal(const T &a, const T &b);
 
 } // namespace utils
-
 } // namespace sba
