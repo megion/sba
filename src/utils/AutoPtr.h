1afee93a09f82551fa65781f6ee2bd1e9338a9ca
Ilya Zadorozhny <ilya.zadorozhny@gmail.com>
2019-04-03 19:11:09 +0300

    add comments for auto_ptr class

--- src/utils/AutoPtr.h
+++ src/utils/AutoPtr.h
@@ -5,12 +5,21 @@ namespace utils {
 
 /**
  * Demo auto_ptr from Ctraustrup C++ book (head 14.4)
+ * Support Resource Acquisition is Initialization technic
+ *
+ * The auto_ptr is deprecated. Instead, use unique_ptr 
+ *
+ * auto_ptr have to use only pointer linked with scalar object. You must not 
+ * use it for pointer linked with array (new T[length]).
+ * 
+ * For array use vector, for exmaple: vector<double> tmp( src.size() );
  */
 template <class X>
 class AutoPtr {
 
     // helper
-    template<class Y> struct AutoPtrRef;
+    template<class Y> struct AutoPtrRef {
+    };
 
     X *ptr_;
 
@@ -21,10 +30,10 @@ public:
 private:
 };
 
-template <class X>
-template <class Y>
-struct AutoPtr<X>::AutoPtrRef<Y> {
-};
+//template <class X>
+//template <class Y>
+//struct AutoPtr<X>::AutoPtrRef<Y> {
+//};
 
 } // namespace utils
 } // namespace sba
