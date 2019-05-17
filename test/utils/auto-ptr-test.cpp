b891f3439fe3187d53adb6ad3566f05fcebebd37
Ilya Zadorozhny <ilya.zadorozhny@gmail.com>
2019-05-17 18:27:28 +0300

    change test

--- test/utils/auto-ptr-test.cpp
+++ test/utils/auto-ptr-test.cpp
@@ -5,28 +5,25 @@ namespace test {
 
 static void test_convert() {
     Child* child = new Child();
-    //sba::utils::AutoPtr<Child> p_child = child; // Erro it doesn't work
+    //sba::utils::AutoPtr<Child> p_child = child; // Error it doesn't work
     sba::utils::AutoPtr<Child> p_child(child); // OK it works
 
     /* 
      * TODO error. It doesn't work because there is not AutoPtr(const AutoPtr &a)
-     * AutoPtr<Parent> px = &x => 
-     * const AutoPtr<Parent> temp(&x); 
-     * AutoPtr<Parent> px(&x); // there is not
      */
     //sba::utils::AutoPtr<Parent> p_parent = p_child;
     
     // OK convert Child* to Parent*
-    //sba::utils::AutoPtr<Parent> p_parent(p_child);
+    sba::utils::AutoPtr<Parent> p_parent(p_child);
 
-    //my_assert(p_parent.get() != nullptr);
-    //my_assert(p_child.get() == nullptr);
+    my_assert(p_parent.get() != nullptr);
+    my_assert(p_child.get() == nullptr);
 }
 
 
 static void test_std_auto_ptr() {
     Child* child = new Child();
-    //sba::utils::AutoPtr<Child> p_child = child; // Erro it doesn't work
+    //sba::utils::AutoPtr<Child> p_child = child; // Error it doesn't work
     std::auto_ptr<Child> p_child(child); // OK it works
 
     //std::auto_ptr<Child> p_child2(child);
