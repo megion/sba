e95a8112bc5ebf85502382c2178b72097392bf3a
Ilya Zadorozhny <ilya.zadorozhny@gmail.com>
2019-05-22 20:01:21 +0300

    small test changes

--- test/utils/auto-ptr-test.cpp
+++ test/utils/auto-ptr-test.cpp
@@ -8,17 +8,11 @@ static void test_convert() {
     //sba::utils::AutoPtr<Child> p_child = child; // Error it doesn't work
     sba::utils::AutoPtr<Child> p_child(child); // OK it works
 
-    /* 
-     * TODO error. It doesn't work because there is not AutoPtr(const AutoPtr &a)
-     */
-    //sba::utils::AutoPtr<Parent> p_parent = p_child;
-    
     // OK convert Child* to Parent*
     sba::utils::AutoPtr<Parent> p_parent(p_child);
 
     my_assert(p_parent.get() != nullptr);
     my_assert(p_child.get() == nullptr);
-
 
     sba::utils::AutoPtr<Parent> p_parent2 = p_parent;
 
@@ -26,32 +20,33 @@ static void test_convert() {
     my_assert(p_parent.get() == nullptr);
 
     // downcast
-    sba::utils::AutoPtr<Child> p_child2(p_parent2);
+    //sba::utils::AutoPtr<Child> p_child2(p_parent2);
 }
 
-static void test_std_auto_ptr() {
-    Child* child = new Child();
-    //sba::utils::AutoPtr<Child> p_child = child; // Error it doesn't work
-    std::auto_ptr<Child> p_child(child); // OK it works
+//static void test_std_auto_ptr() {
+    //Child* child = new Child();
+    ////std::auto_ptr<Child> p_child = child; // Error it doesn't work
+    //std::auto_ptr<Child> p_child(child); // OK it works
 
-    // OK convert Child* to Parent*
-    std::auto_ptr<Parent> p_parent(p_child);
+    //// OK convert Child* to Parent*
+    //std::auto_ptr<Parent> p_parent(p_child);
 
-    my_assert(p_parent.get() != nullptr);
-    my_assert(p_child.get() == nullptr);
+    //my_assert(p_parent.get() != nullptr);
+    //my_assert(p_child.get() == nullptr);
 
-    std::auto_ptr<Parent> p_parent2 = p_parent;
+    //std::auto_ptr<Parent> p_parent2 = p_parent;
 
-    my_assert(p_parent2.get() != nullptr);
-    my_assert(p_parent.get() == nullptr);
+    //my_assert(p_parent2.get() != nullptr);
+    //my_assert(p_parent.get() == nullptr);
 
-    //std::auto_ptr<Child> p_child2(p_parent2);
-}
+    //// downcast doesn't work for std::auto_ptr
+    ////std::auto_ptr<Child> p_child2(p_parent2);
+//}
 
 void auto_ptr_test() {
     suite("AutoPtr");
     mytest(convert);
-    mytest(std_auto_ptr);
+    //mytest(std_auto_ptr);
 }
 
 } // namespace test
