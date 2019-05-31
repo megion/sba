997611f38dde4b2eaa54de2070374f4c2e0a4077
Ilya Zadorozhny <ilya.zadorozhny@gmail.com>
2019-05-31 19:13:48 +0300

    memory allocator testing

--- test/utils/allocator-test.cpp
+++ test/utils/allocator-test.cpp
@@ -8,8 +8,43 @@ void* operator new(size_t sz, test::Arena* a) {
 namespace test {
 
 static void test_allocate_memory() {
+    Arena* persistent = new Persistent();
+    char* d1 = (char*) persistent->alloc(2);
+    d1[0] = 'a';
+    d1[1] = 'b';
 
-    //my_assert(p_parent.get() == nullptr);
+    char* d2 = (char*) persistent->alloc(3);
+    d2[0] = 'c';
+    d2[1] = 'd';
+    d2[2] = 'f';
+
+    char* d3 = (char*) persistent->alloc(2);
+    d3[0] = 'g';
+    d3[1] = 'h';
+
+    my_assert(d1[0] == 'a');
+    my_assert(d1[1] == 'b');
+
+    my_assert(d2[0] == 'c');
+    my_assert(d2[1] == 'd');
+    my_assert(d2[2] == 'f');
+
+    my_assert(d3[0] == 'g');
+    my_assert(d3[1] == 'h');
+
+    persistent->free(d2, 3);
+
+    /* 
+     * TODO: update pointers, but it is hack. 
+     */
+    d3 = d3 - 3;
+
+    my_assert(d1[0] == 'a');
+    my_assert(d1[1] == 'b');
+    my_assert(d3[0] == 'g');
+    my_assert(d3[1] == 'h');
+
+    delete persistent;
 }
 
 void allocator_test() {
--- test/utils/allocator-test.h
+++ test/utils/allocator-test.h
@@ -10,21 +10,39 @@ class Arena {
 public:
     virtual void *alloc(size_t) = 0;
     virtual void free(void *, size_t) = 0;
+
+    virtual ~Arena() {};
 };
 
 class Persistent : public Arena {
 public:
     Persistent() : len_(0) {}
 
+    ~Persistent() {}
+
 public:
     void *alloc(size_t sz) {
+        size_t l = len_;
         len_ += sz;
-        return &buff_[len_];
+        return (buff_ + l);
     };
 
     void free(void *b, size_t sz) {
-        void *dest = b + sz;
-        std::memcpy(b, dest, len_ - (b + sz - buff_));
+        /*
+         * TODO: move data from endRemovingData to startRemovingData
+         * void * destination, const void * source, size_t num
+         *
+         * data view:
+         * 0----startRemoving---endRemoving-----len_
+         *
+         * after free:
+         * 0---------len_
+         */
+        char *startRemovingData = (char *)b;
+        char *endRemovingData = startRemovingData + sz;
+        std::memcpy(startRemovingData, endRemovingData,
+                    len_ - (endRemovingData - buff_));
+        len_ -= sz;
     };
 
 private:
